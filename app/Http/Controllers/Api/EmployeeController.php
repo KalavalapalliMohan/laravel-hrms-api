e9803f7b89e040e313e3da8bcdb16f0cef1e54cd
Kalavalapalli Mohan <kalavalapallimohan9@gmail.com>
2026-02-22 13:03:56 +0530

    Added search, filter, and sort functionality to Employee APIs

--- app/Http/Controllers/Api/EmployeeController.php
+++ app/Http/Controllers/Api/EmployeeController.php
@@ -13,11 +13,33 @@ use App\Http\Resources\EmployeeResource;
 class EmployeeController extends Controller
 {
     use AuthorizesRequests;
-    
-    public function index()
+
+    public function index(Request $request)
     {
         $this->authorize('viewAny', Employee::class);
-        return EmployeeResource::collection(Employee::paginate(10));
+
+        $employees = Employee::query()
+
+            ->when($request->search, function ($q) use ($request) {
+                $q->where('name', 'like', '%' . $request->search . '%')
+                ->orWhere('email', 'like', '%' . $request->search . '%');
+            })
+
+            ->when($request->designation, function ($q) use ($request) {
+                $q->where('designation', $request->designation);
+            })
+
+            ->when($request->sort === 'latest', function ($q) {
+                $q->latest();
+            })
+
+            ->when($request->sort === 'oldest', function ($q) {
+                $q->oldest();
+            })
+
+            ->paginate(10);
+
+        return EmployeeResource::collection($employees);
     }
 
     public function store(StoreEmployeeRequest $request)
